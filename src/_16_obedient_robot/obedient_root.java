b47c45e74f9b3f03bfe509947b906852a9f61432
MichaelYeh507 <54426940+MichaelYeh507@users.noreply.github.com>
2019-11-08 17:05:59 -0800

    obedient_root updated

--- src/_16_obedient_robot/obedient_root.java
+++ src/_16_obedient_robot/obedient_root.java
@@ -10,8 +10,9 @@ public class obedient_root {
 
 	static Robot rob = new Robot();
 	static String color = JOptionPane.showInputDialog("Pick a color, blue, red or yellow?");
+	static String shape = JOptionPane.showInputDialog("What shape do you want to draw?");
 	public static void main(String[] args) {
-		String shape = JOptionPane.showInputDialog("What shape do you want to draw?");
+		setColor();
 		if (shape.equalsIgnoreCase("square")) {
 			drawSquare();
 		} else if (shape.equalsIgnoreCase("triangle")) {
@@ -22,13 +23,6 @@ public class obedient_root {
 	}
 	//draw a square
 	static void drawSquare() {
-		if (color.equalsIgnoreCase("blue")) {
-			rob.setPenColor(Color.blue);
-		} else if (color.equalsIgnoreCase("red")) {
-			rob.setPenColor(Color.red);
-		} else {
-			rob.setPenColor(Color.yellow);
-		}
 		rob.setSpeed(10);
 		rob.penDown();
 		for (int i = 0; i < 4; i++) {
@@ -38,13 +32,6 @@ public class obedient_root {
 	}
 	//draw a triangle
 	static void drawTriangle() {
-		if (color.equalsIgnoreCase("blue")) {
-			rob.setPenColor(Color.blue);
-		} else if (color.equalsIgnoreCase("red")) {
-			rob.setPenColor(Color.red);
-		} else {
-			rob.setPenColor(Color.yellow);
-		}
 		rob.setSpeed(10);
 		rob.penDown();
 		for (int i = 0; i < 3; i++) {
@@ -54,18 +41,21 @@ public class obedient_root {
 	}
 	//draw a circle
 	static void drawCircle() {
+		rob.setSpeed(10);
+		rob.penDown();
+		for (int i = 0; i < 90; i++) {
+			rob.move(4);
+			rob.turn(4);
+		}
+	}
+	
+	static void setColor() {
 		if (color.equalsIgnoreCase("blue")) {
 			rob.setPenColor(Color.blue);
 		} else if (color.equalsIgnoreCase("red")) {
 			rob.setPenColor(Color.red);
 		} else {
 			rob.setPenColor(Color.yellow);
-		}
-		rob.setSpeed(10);
-		rob.penDown();
-		for (int i = 0; i < 90; i++) {
-			rob.move(4);
-			rob.turn(4);
 		}
 	}
 }
